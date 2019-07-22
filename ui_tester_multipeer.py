80ab3637daa43bcee93eb19fb4e598a1afdf047c
Mikael <mikael.honkala@gmail.com>
2019-07-22 17:22:42 +0300

    Simpler version

--- ui_tester_multipeer.py
+++ ui_tester_multipeer.py
@@ -12,17 +12,17 @@ from tinysync.conduit.multipeer import MultipeerConduit
 v = CloseableView(background_color=.7)
 
 one = SeaOfBalls(MultipeerConduit())
-two = SeaOfBalls(MultipeerConduit())
-three = SeaOfBalls(MultipeerConduit())
-four = SeaOfBalls(MultipeerConduit())
+#two = SeaOfBalls(MultipeerConduit())
+#three = SeaOfBalls(MultipeerConduit())
+#four = SeaOfBalls(MultipeerConduit())
 
 grid = GridView(pack=GridView.FILL, frame=v.bounds, flex='WH')
 v.add_subview(grid)
 
 grid.add_subview(one)
-grid.add_subview(two)
-grid.add_subview(three)
-grid.add_subview(four)
+#grid.add_subview(two)
+#grid.add_subview(three)
+#grid.add_subview(four)
 
 v.present()
 
